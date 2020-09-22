151ffd213d7f6bfa4dbb6740d4943f3d51c0751a
Sanaxen <bccatfalcon@gmail.com>
2020-09-22 21:18:08 +0900

    add UI application example

---
+++ face_recognition/face_recognition/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace face_recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "movie(*.mp4,*.avi,*mpeg|*.mp4;*.avi;*.mpeg";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            this.openFileDialog2.Filter = "movie(*.png,*.jpg|*.png;*.jpg";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1101, 413);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.numericUpDown1);
+            this.panel3.Controls.Add(this.button5);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(907, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(194, 413);
+            this.panel3.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "camera\r\nrecognition";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 70);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "movie file\r\nrecognition";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(179, 70);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "image file\r\nrecognition";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 337);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 70);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "add user";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitter2);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1101, 413);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 416);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1101, 100);
+            this.panel2.TabIndex = 5;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitter1.Location = new System.Drawing.Point(0, 413);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(1101, 3);
+            this.splitter1.TabIndex = 6;
+            this.splitter1.TabStop = false;
+            // 
+            // splitter2
+            // 
+            this.splitter2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter2.Location = new System.Drawing.Point(904, 0);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(3, 413);
+            this.splitter2.TabIndex = 5;
+            this.splitter2.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(123, 231);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 22);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "camera=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 516);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "face_recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ face_recognition/face_recognition/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace face_recognition
+{
+    public partial class Form1 : Form
+    {
+        public static string MyPath = "";
+        public static string cuDir = @"C:\Users\bccat\Desktop\dnn_face_recognition_ex\bin";
+        System.Diagnostics.ProcessStartInfo app = null;
+        System.Diagnostics.Process process = null;
+
+        int image_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            MyPath = System.AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        public static System.Drawing.Image CreateImage(string filename)
+        {
+            System.Drawing.Image img = null;
+            try
+            {
+                System.IO.FileStream fs = new System.IO.FileStream(
+                    filename,
+                    System.IO.FileMode.Open,
+                    System.IO.FileAccess.Read);
+                img = System.Drawing.Image.FromStream(fs);
+                fs.Close();
+            }
+            catch
+            {
+            }
+            return img;
+        }
+
+        void processClose(System.Diagnostics.Process process)
+        {
+            if (process == null) return;
+            if (!process.HasExited)
+            {
+                process.Kill();
+                //KillProcessTree(process);
+                process = null;
+            }
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear()
+        {
+            for ( int i = 0; i < 10000; i++)
+            {
+                string filename = cuDir + "\\tmp\\tmp_" + $"{i:00000}" + ".png";
+                if (System.IO.File.Exists(filename))
+                {
+                    System.IO.File.Delete(filename);
+                }
+            }
+
+            int count = 10000;
+            do
+            {
+                string filename = cuDir + "\\tmp\\tmp_" + $"{count:00000}" + ".png";
+                if (System.IO.File.Exists(filename))
+                {
+                    System.IO.File.Delete(filename);
+                    count++;
+                }else
+                {
+                    break;
+                }
+            } while (true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.IO.Directory.SetCurrentDirectory(cuDir);
+            pictureBox1.Image = null;
+            app = new System.Diagnostics.ProcessStartInfo();
+
+            clear();
+
+            app.FileName = @"cuda\\dnn_face_recognition_ex.exe";
+            app.Arguments = " " + "--no_show 1";
+            app.Arguments += " " + "--camID " + numericUpDown1.Value.ToString();
+            app.Arguments += " " + "--recog";
+            app.UseShellExecute = true;
+            app.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+
+            timer1.Start();
+            process = System.Diagnostics.Process.Start(app);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string filename = cuDir+"\\tmp\\tmp_" +$"{image_count:00000}" + ".png";
+            if ( System.IO.File.Exists(filename))
+            {
+                try
+                {
+                    var tmp = pictureBox1.Image;
+                    pictureBox1.Image = CreateImage(filename);
+                    if (pictureBox1.Image == null)
+                    {
+                        pictureBox1.Image = tmp;
+                    }
+                    else
+                    {
+                        System.IO.File.Delete(filename);
+                        image_count++;
+                    }
+                }
+                catch { }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string filename = cuDir + "\\break.run";
+            try
+            {
+                if (System.IO.File.Exists(filename))
+                {
+                    System.IO.File.Delete(filename);
+                }
+                System.IO.File.Create(filename);
+            }
+            catch { }
+
+            processClose(process);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if ( openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            System.IO.Directory.SetCurrentDirectory(cuDir);
+            pictureBox1.Image = null;
+            app = new System.Diagnostics.ProcessStartInfo();
+
+            clear();
+
+            app.FileName = @"cuda\\dnn_face_recognition_ex.exe";
+            app.Arguments = " " + "--no_show 1";
+            app.Arguments += " " + "--video";
+            app.Arguments += " " + openFileDialog1.FileName;
+            app.Arguments += " " + "--recog";
+            app.UseShellExecute = true;
+            app.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+
+            timer1.Start();
+            process = System.Diagnostics.Process.Start(app);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            System.IO.Directory.SetCurrentDirectory(cuDir);
+            pictureBox1.Image = null;
+            app = new System.Diagnostics.ProcessStartInfo();
+
+            clear();
+            string filename = cuDir + "\\output.png";
+            if (System.IO.File.Exists(filename))
+            { 
+                System.IO.File.Delete(filename);
+            }
+            pictureBox1.Image = null;
+
+            app.FileName = @"cuda\\dnn_face_recognition_ex.exe";
+            app.Arguments = " " + "--no_show 1";
+            app.Arguments += " " + "--image";
+            app.Arguments += " " + openFileDialog2.FileName;
+            app.Arguments += " " + "--recog";
+            app.UseShellExecute = true;
+            app.WindowStyle = System.Diagnostics.ProcessWindowStyle.Minimized;
+
+            timer1.Stop();
+            process = System.Diagnostics.Process.Start(app);
+            process.WaitForExit();
+
+            if (System.IO.File.Exists(filename))
+            {
+                try
+                {
+                    var tmp = pictureBox1.Image;
+                    pictureBox1.Image = CreateImage(filename);
+                }
+                catch { }
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            System.IO.Directory.SetCurrentDirectory(cuDir);
+            pictureBox1.Image = null;
+            app = new System.Diagnostics.ProcessStartInfo();
+
+            clear();
+            string filename = cuDir + "\\output.png";
+            if (System.IO.File.Exists(filename))
+            {
+                System.IO.File.Delete(filename);
+            }
+            pictureBox1.Image = null;
+
+            app.FileName = @"cuda\\dnn_face_recognition_ex.exe";
+            app.Arguments = " " + openFileDialog2.FileName;
+            app.UseShellExecute = false;
+
+            timer1.Stop();
+            pictureBox1.Image = CreateImage(openFileDialog2.FileName);
+            process = System.Diagnostics.Process.Start(app);
+            process.WaitForExit();
+        }
+    }
+}
